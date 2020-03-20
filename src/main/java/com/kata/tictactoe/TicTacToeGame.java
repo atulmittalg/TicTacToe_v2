6e9148e61cb0210fdf00895cfc4bf148a0b6e6ab
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-20 17:07:13 +0100

    Update: Making method parameters as Final.

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -3,12 +3,12 @@ package com.kata.tictactoe;
 public class TicTacToeGame {
     private char[][] gameboardLayout = new char[3][3];
 
-    public char getTokenAtPosition(int positionX, int positionY) {
+    public char getTokenAtPosition(final int positionX, final int positionY) {
         if(gameboardLayout[positionX][positionY] == 'O'){return 'O';}
         return 'X';
     }
 
-    public void markTokenAt(int positionX, int positionY) {
+    public void markTokenAt(final int positionX, final int positionY) {
         gameboardLayout[positionX][positionY] = 'O';
     }
 }
